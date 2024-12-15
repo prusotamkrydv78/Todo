027820e58242db99fc1ebbd95d21e972dfeba17b
prusotamkrydv78 <prusotamkumaryadav78@gmail.com>
2024-12-15 13:23:39 +0545

    completed or not completed fun added

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,7 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, numberAttribute } from '@angular/core';
+import { Component, inject, numberAttribute } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+import { PanelModule } from 'primeng/panel';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { LoginUserService } from '../../services/login-user.service';
+import {  CheckboxModule } from 'primeng/checkbox';
 interface tasksListSchema {
   data: any;
   message: string;
@@ -10,12 +17,20 @@ interface tasksListSchema {
 }
 @Component({
   selector: 'app-tasks',
-  imports: [FormsModule, CommonModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    PanelModule,
+    AvatarModule,
+    ButtonModule,
+    MenuModule,
+    CheckboxModule,
+  ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  tasksList: tasksListSchema;
+  tasksList: any;
 
   isDialogVisible = false;
 
@@ -24,18 +39,51 @@ export class TasksComponent {
     title: '',
     description: '',
   };
+  loginUserService = inject(LoginUserService);
+  items = [
+    {
+      label: 'Refresh',
+      icon: 'pi pi-refresh',
+    },
+    {
+      label: 'Search',
+      icon: 'pi pi-search',
+    },
+    {
+      separator: true,
+    },
+    {
+      label: 'Delete',
+      icon: 'pi pi-times',
+    },
+  ];
   constructor(private http: HttpClient) {}
   ngOnInit() {
     // want to call api
     this.loadAllTasks();
   }
   loadAllTasks() {
+    this.http.get('http://localhost:3000/tasks').subscribe((res: []) => {
+      if (res) {
+        this.tasksList = res.filter(
+          (task: any) => task.userId == this.loginUserService.loginUser.id
+        );
+        console.log(this.tasksList);
+      }
+    });
+  }
+
+  onCompleted(id: number) {
+    let task = this.tasksList.find((task: any) => task.id === id); 
+console.log(task);
+
     this.http
-      .get('https://api.freeapi.app/api/v1/todos')
-      .subscribe((res: any) => {
-        if (res) {
-          this.tasksList = res;
-        }
+      .patch(`http://localhost:3000/tasks/${id}`,task )
+      .subscribe((res) => { 
+          if (res) {
+            // console.log(res);
+            this.loadAllTasks();
+          } 
       });
   }
   onDelete(id: number) {
@@ -52,10 +100,9 @@ export class TasksComponent {
       });
   }
   onEdit(id: number) {
-    
-  this.isDialogVisible = true;
+    this.isDialogVisible = true;
 
-  this.showDialog = true;
+    this.showDialog = true;
     let task = this.tasksList.data.filter((task) => {
       return task._id == id;
     });
@@ -63,20 +110,19 @@ export class TasksComponent {
     this.taskData.description = task[0].description;
     console.log(task[0]);
   }
-  onUpdate(id:number) {
-    
-  this.isDialogVisible = false;
+  onUpdate(id: number) {
+    this.isDialogVisible = false;
 
-  this.showDialog = false;
-  this.http.patch(`https://api.freeapi.app/api/v1/todos/${id}`,this.taskData).subscribe((res)=>{
-    if(res){
-      if(res){
-        console.log(res);
-        
-      }
-      this.loadAllTasks();
-    }
-  })
-
+    this.showDialog = false;
+    this.http
+      .patch(`https://api.freeapi.app/api/v1/todos/${id}`, this.taskData)
+      .subscribe((res) => {
+        if (res) {
+          if (res) {
+            console.log(res);
+          }
+          this.loadAllTasks();
+        }
+      });
   }
 }
